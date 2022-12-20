9599dd198766a7c931946958f5d45ec4b0214c7a
juhyunO <120620959+juhyunO@users.noreply.github.com>
2022-12-20 11:37:38 +0900

    Add files via upload

--- bingo/urls.py
+++ bingo/urls.py
@@ -7,6 +7,5 @@ urlpatterns =[
     path('detail1/',views.Detail1,name='detail1'),
     path('detail2/',views.Detail2,name='detail2'),
     path('detail3/',views.Detail3,name='detail3'),
-    path('detail4/',views.Detail4,name='detail4'),
-    path('',views.Bingo,name='bingo')
+    path('',views.Bingo,name='bingo'),
 ]
